4f5fcdb77214f5ae9fda7022f9df25662a35993a
Royce Boone <royceboone@hotmail.com>
2023-12-07 13:18:19 -0800

    lorem ipsum

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
       <h1>Rick & Morty</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et perferendis
+        in optio! Nostrum et, debitis laborum harum maxime fuga praesentium?
+      </p>
     </div>
   );
 }
